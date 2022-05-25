66f047dbd3f8c68d7a409c52e3ecdcf659a50809
dgrushetskiy <dgrushetskiy@gmail.com>
2022-05-26 01:19:25 +0300

    add find top elements array

--- introduction-algorithms/src/main/java/ru/gothmog/example/ArrayMax.java
+++ introduction-algorithms/src/main/java/ru/gothmog/example/ArrayMax.java
@@ -1,8 +1,12 @@
 package ru.gothmog.example;
 
+import java.util.Arrays;
+
 public class ArrayMax {
     public static void main(String[] args) {
-
+        int[] ages = {10, 12, 45, 34, 109, 9, 75, 32, 1, 3, 96, 43, 105};
+        int[] top3ValueAges = findTopElement(ages, 13);
+        System.out.println(Arrays.toString(top3ValueAges));
     }
 
     private static int findMaxUnderBoundary(int[] inputArray, int topBoundary) {
@@ -18,6 +22,13 @@ public class ArrayMax {
     private static int[] findTopElement(int[] inputArray, int numberOfElements){
         int [] topElements = new int[numberOfElements];
 
+        int previousMax = Integer.MAX_VALUE;
+
+        for (int i = 0; i < numberOfElements; i++) {
+            int currentMax = findMaxUnderBoundary(inputArray, previousMax);
+            previousMax = currentMax;
+            topElements[i] = currentMax;
+        }
         return topElements;
     }
 }
